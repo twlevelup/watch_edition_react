fe82050ee659cabc74f337a00fe2b55e59ee12df
Sinan Aumarah <saumarah@thoughtworks.com>
2017-07-21 09:33:46 +1000

    Renamed the component from HomeContainer to HomePage

--- src/app/components/HomePage/HomePage.jsx
+++ src/app/components/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Watch from '../Watch/Watch';
 import './HomePage.scss';
 
-export default class HomeContainer extends React.Component {
+export default class HomePage extends React.Component {
     render() {
         return (
           <div id='home-container'>
