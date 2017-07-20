a9555ff2bad31559c342e555bf6801293a76c920
Sinan Aumarah <saumarah@thoughtworks.com>
2017-07-20 13:35:59 +1000

    + Added unit tests

---
+++ src/app/pages/HomeScreen/HomeScreen.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import HomeScreen from './HomeScreen';
+
+describe('HomeScreen component', () => {
+  const componentWrapper = mount(
+    <HomeScreen />
+  );
+
+  test('it should have DateTimeDisplay component', () => {
+    expect(componentWrapper.find('#date-time-display').exists()).toBeTruthy();
+  });
+
+  test('it should have some content', () => {
+    expect(componentWrapper.find('#home-page-content').exists()).toBeTruthy();
+  });
+
+});
+
