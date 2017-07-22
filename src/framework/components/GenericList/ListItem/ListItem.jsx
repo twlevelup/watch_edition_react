b65ff804528b8afefa3e4d7408c7e180ebdf243a
Ray Tung <raytung@users.noreply.github.com>
2017-07-22 19:32:16 +1000

    Added ListItem component

---
+++ src/framework/components/GenericList/ListItem/ListItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { string } from 'prop-types';
+
+const ListItem = ({ className = '', label, text }) => (
+  <span className={className}>
+    <span className="key">{label}</span>
+    <span className="value">{text}</span>
+  </span>
+);
+
+ListItem.propType = {
+  label: string.isRequired,
+  text: string.isRequired,
+};
+
+export default ListItem;
---
+++ src/framework/components/GenericList/ListItem/ListItem.spec.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ListItem from './ListItem';
+
+describe('List Item', () => {
+  const composeComponent = ({ label, text }) => shallow(<ListItem label={label} text={text}/>);
+
+  test('It displays label and text', () => {
+    const items = [
+      { label: 'labelA', text: 'some text'},
+      { label: 'labelB', text: 'some other text'},
+    ];
+    items.forEach((item) => {
+      const component = composeComponent(item);
+      expect(component.find('.key').text()).toEqual(item.label);
+      expect(component.find('.value').text()).toEqual(item.text);
+    });
+  });
+});
