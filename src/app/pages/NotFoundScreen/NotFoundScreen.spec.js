4ff0fcb67eab533f865d372f76467b4b1e0d6caa
Sinan Aumarah <sinan.aumarah@gmail.com>
2017-07-23 20:21:51 +1000

    Refactored test

--- src/app/pages/NotFoundScreen/NotFoundScreen.spec.js
+++ src/app/pages/NotFoundScreen/NotFoundScreen.spec.js
@@ -3,12 +3,8 @@ import { shallow } from 'enzyme';
 import NotFoundScreen from './NotFoundScreen';
 
 describe('NotFoundScreen component', () => {
-  const componentWrapper = shallow(
-    <NotFoundScreen />
-  );
-
   test('it should has some text telling you made a bad decision', () => {
-    expect(componentWrapper.find('#not-found-page').text()).toContain('Oh Noes!');
+    expect(shallow(<NotFoundScreen />).find('#not-found-page').text()).toContain('Oh Noes!');
   });
 });
 
