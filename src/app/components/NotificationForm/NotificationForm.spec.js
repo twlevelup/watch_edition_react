c488c4ebab84d9eeaff3aae83b7e5c9af16c5c9f
Sinan Aumarah <sinan.aumarah@gmail.com>
2017-07-23 17:52:52 +1000

    Improved unit test coverage. Added state and callback verifications

--- src/app/components/NotificationForm/NotificationForm.spec.js
+++ src/app/components/NotificationForm/NotificationForm.spec.js
@@ -20,5 +20,39 @@ describe('NotificationForm', () => {
     const componentWrapper = shallow(<NotificationForm { ...defaultProps } />);
     expect(componentWrapper.find('.submit-btn').exists()).toBeTruthy();
   });
+
+  describe('When notification text is changed', () => {
+    test('it should save new text in the component\'s state ', () => {
+      const componentWrapper = shallow(<NotificationForm { ...defaultProps } />);
+      const newText = 'HelloWorld';
+      componentWrapper.find('.notification-input').simulate('change', { target: { value: newText } });
+      expect(componentWrapper.state().input).toBe(newText);
+    });
+  });
+
+  describe('When send notification button is clicked', () => {
+    const submissionEvent = {
+      preventDefault: jest.fn(),
+    };
+    const composeFormAndSimulateSubmission = () => {
+      const componentWrapper = shallow(<NotificationForm { ...defaultProps } />);
+      componentWrapper.find('#notification-form').simulate('submit', submissionEvent);
+    };
+
+    test('it should pass the current textarea value to to the callback function', () => {
+      composeFormAndSimulateSubmission();
+      expect(defaultProps.handleEvent).toBeCalledWith({ displayNotification: true, text: defaultProps.defaultText });
+    });
+
+    test('it should pass displayNotification set to True to to the callback function', () => {
+      composeFormAndSimulateSubmission();
+      expect(defaultProps.handleEvent).toBeCalledWith({ displayNotification: true, text: defaultProps.defaultText });
+    });
+
+    test('it should not refresh the page', () => {
+      composeFormAndSimulateSubmission();
+      expect(submissionEvent.preventDefault).toHaveBeenCalled();
+    });
+  });
 });
 
