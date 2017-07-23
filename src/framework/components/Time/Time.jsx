728c61fc950c0e095776359db962b29f9bf3ab17
Andrew Watson <awatson@thoughtworks.com>
2017-07-23 13:56:11 +1000

    Cleanup time component

--- src/framework/components/Time/Time.jsx
+++ src/framework/components/Time/Time.jsx
@@ -1,14 +1,24 @@
 import Moment from 'react-moment';
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import './time.scss';
 
-export default class Watch extends React.Component {
-  render() {
-    let format = this.props.format || 'HH:mm:ss';
-    return (
-      <div id='time-display'>
-        <p className='clock-time'>Time: <Moment interval={1000} format={format}></Moment></p>
-      </div>
-    );
-  }
-}
+const Time = (props) => {
+  const { format } = props;
+  return (
+    <div id='time-display'>
+      <p className='clock-time'>Time: <Moment interval={ 1000 } format={ format } /></p>
+    </div>
+  );
+};
+
+Time.defaultProps = {
+  format: 'HH:mm:ss',
+};
+
+Time.propTypes = {
+  format: PropTypes.string,
+};
+
+export default Time;
