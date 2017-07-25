9c9d4c338008f74cb4e7eff34e271213e02ac7b6
yundifu <mymedicine77@gmail.com>
2017-07-25 11:55:30 +1000

    fixed contactlist screen error

--- src/app/pages/ContactListScreen/ContactListScreen.jsx
+++ src/app/pages/ContactListScreen/ContactListScreen.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import GenericList from '../../../framework/components/GenericList/GenericList';
 import './contact_list.css';
 
-const ContactListScreen = (contacts) => {
+const ContactListScreen = ({ contacts }) => {
   return (
     <div id='contact-screen' className='contact-screen'>
       <h1 className='title'>Contacts </h1>
@@ -13,8 +13,8 @@ const ContactListScreen = (contacts) => {
   );
 };
 
-ContactListScreen.contacts = {
-  format: PropTypes.arrayOf(PropTypes.object).isRequired,
+ContactListScreen.propTypes = {
+  contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default ContactListScreen;
