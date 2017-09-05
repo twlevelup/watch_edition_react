fbb1214cd06dc7f55b0920352ad7a744ccf94833
Aaron Edwards <aedwards@thoughtworks.com>
2017-09-05 13:51:53 +1000

    :hammer: moved more boilerplate into watch index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import { Provider } from 'react-redux';
-import { routerMiddleware, routerReducer } from 'react-router-redux';
-import browserHistory from './framework/Router/BrowserHistory';
-
-import ButtonActionsReducer from './framework/reducers/ButtonsRemapReducer';
-import NotificationReducer from './framework/reducers/NotificationReducer';
 
 import HomeScreen from './app/pages/HomeScreen/HomeScreen';
 import CounterScreen from './app/pages/CounterScreen/CounterScreen';
 import ContactScreen from './app/pages/ContactListScreen/ContactListScreen';
 import NotFoundScreen from './app/pages/NotFoundScreen/NotFoundScreen';
 import contacts from './app/data/contacts.json';
-import WatchApp from './framework/components/WatchApp/WatchApp';
-
-const middleware = routerMiddleware(browserHistory);
-
-const reducers = combineReducers({
-  ButtonActionsReducer,
-  router: routerReducer,
-  NotificationReducer,
-});
-
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export const store = createStore(reducers, composeEnhancers(applyMiddleware(middleware)));
+import WatchApp from './framework';
 
 const pages = [
   { path: '/', Component: HomeScreen },
@@ -35,6 +16,5 @@ const pages = [
 ];
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <WatchApp pages={ pages } />
-  </Provider>, document.getElementById('root'));
+  <WatchApp pages={ pages } />,
+  document.getElementById('root'));
