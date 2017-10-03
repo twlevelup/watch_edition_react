697e343515eb8676dc9bac682ab90ddbf6d08a53
Aaron Edwards <aedwards@thoughtworks.com>
2017-10-03 18:25:29 +1100

    Revert "Added the ability to select a contact and view their details (rework of #28)" (#30)

--- src/app/pages/ContactListScreen/ContactListScreen.jsx
+++ src/app/pages/ContactListScreen/ContactListScreen.jsx
@@ -2,55 +2,41 @@ import React from 'react';
 import {
   string,
   arrayOf,
-  number,
   shape,
 } from 'prop-types';
 
-import ScrollList from '../../../framework/components/ScrollList/ScrollList';
+import GenericList from '../../../framework/components/GenericList/GenericList';
 import WithButtonConfigs from '../../../framework/containers/WithButtonConfigs';
 import ButtonAction from '../../../framework/util/ButtonAction';
+import Contact from './components/Contact/Contact';
 import './contact_list.css';
 
-export const ContactListComponent = ({ contacts, selectedIndex }) => (
-  <div id='contact-screen' className='contact-screen'>
-    <h1 className='title'>Contacts</h1>
-    <ScrollList
-      labels={ contacts.map(c => c.name) }
-      selectedIndex={ selectedIndex }
-    />
-  </div>
-);
-
-const getNextIndex = (indexChange, selectedIndex, contacts) => {
-  const newIndex = selectedIndex + indexChange;
-  return Math.abs((newIndex + contacts.length) % contacts.length);
+export const ContactListScreen = ({ contacts }) => {
+  return (
+    <div id='contact-screen' className='contact-screen'>
+      <h1 className='title'>Contacts</h1>
+      <GenericList
+        className='contacts-list'
+        items={ contacts }
+        listItem={ Contact }
+      />
+    </div>
+  );
 };
 
-export const buttonActions = ({ contacts, selectedIndex = 0 }) => ({
-  RIGHT: () => ButtonAction.goToPage('/counter'),
-  LEFT: () => ButtonAction.goToPage('/'),
-  BOTTOM: () => {
-    ButtonAction.goToPage({ state: { selectedIndex: getNextIndex(1, selectedIndex, contacts) } });
-  },
-  TOP: () => {
-    ButtonAction.goToPage({ state: { selectedIndex: getNextIndex(-1, selectedIndex, contacts) } });
-  },
-  SCREEN: () => ButtonAction.goToPage({
-    pathname: '/contact-view',
-    state: { contact: contacts[selectedIndex] },
-  }),
-});
-
-ContactListComponent.propTypes = {
-  selectedIndex: number,
+ContactListScreen.propTypes = {
   contacts: arrayOf(shape({
     name: string,
+    phone: string,
+    address: string,
   })).isRequired,
 };
 
-ContactListComponent.defaultProps = {
-  selectedIndex: 0,
+export const ContactScreenButtons = {
+  LEFT: () => ButtonAction.goToPage('/'),
+  RIGHT: () => ButtonAction.goToPage('/counter'),
+  TOP: () => ButtonAction.scrollUp(),
+  BOTTOM: () => ButtonAction.scrollDown(),
 };
 
-
-export default WithButtonConfigs(ContactListComponent, buttonActions);
+export default WithButtonConfigs(ContactListScreen, ContactScreenButtons);
